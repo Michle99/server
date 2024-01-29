6f3c24b462196a4232541a797cc708ca5c20fde5
Michle99 <sammiyonyx@gmail.com>
2024-01-28 19:12:27 -0500

    udpates..

--- dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser, loginUser, logoutUser } from '../controllers/auth.controller';
-const router = express.Router();
-// Registration route
-router.post('/signup', registerUser);
-// Login route
-router.post('/login', loginUser);
-router.post('/logout', logoutUser);
-export default router;
